6a1f431537c0d2e4c288a8b956620cabb747d371
ernynany <doct2wise@gmail.com>
2023-09-07 21:39:26 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h> 
 /**
- * gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out ---> use
 * main - Entry point
 *
-* Retum: Always 0 (Success) 
+* Return: Always 0 (Success) 
 */
-
-int main(void)
-{
+int main(void){
     puts("\"Programming is like bulding a  multilingual puzzle");
     return(0);
 }   
