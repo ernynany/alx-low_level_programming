191483035736739a3e10049f1ce194b81e55c41c
ernynany <doct2wise@gmail.com>
2023-09-07 20:42:23 -0700

    second commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio h> 
+/**
+* main - Entry point
+*
+* Retum: Always 0 (Success) 
+*/
+
+int main(void)
+{
+    printf("Programming is like bulding a  multilingual puzzle\n");
+    return(0);
+}   
