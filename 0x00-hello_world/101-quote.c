db4e45b02edee447f3e9bcc880bc7f66dc318911
ernynany <doct2wise@gmail.com>
2023-09-08 17:30:58 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
-#include <string.h>
 
-int main() {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(message);
-    ssize_t bytes_written = write(2, message, len);
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-    if (bytes_written == len) {
-        return 1;
-    } else {
-        return 0;
-    }
+	return (1);
 }
